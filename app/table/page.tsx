876129323f6f48e18b981942c8f07e9739c301b8
Anshu Patel <anshu@techstuff.cloud>
2024-03-20 15:38:37 +0530

    Shad cn table implementation

---
+++ app/table/page.tsx
@@ -0,0 +1,12 @@
+import TaskPage from '@/components/Table';
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      <TaskPage />
+    </div>
+  );
+};
+
+export default page;
