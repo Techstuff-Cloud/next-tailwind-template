e01fde9d15b167aec092c5c98082cd8584adbceb
aditya-v22 <aditya.vaishanv2022@gmail.com>
2024-03-20 15:20:34 +0530

    Refactor root layout component (moved to (dashboard) route)

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,9 +1,10 @@
 import { SettingContext } from '@/_context/ThemeContext';
 import WrapperComponent from '@/app/demo/shadcn-atoms/WrapperComponent';
+import { SideNavbarComponent } from '@/components/SideNavbar';
+import TopNavbarComponent from '@/components/TopNavbar';
 import { Toaster } from '@/components/ui/toaster';
 import type { Metadata } from 'next';
 import * as React from 'react';
-import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Template Next App',
@@ -15,7 +16,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <SettingContext>
       <WrapperComponent>
         <div className='flex'>
+          <SideNavbarComponent />
           <div className='flex flex-col w-full'>
+            <TopNavbarComponent />
             {children}
             <Toaster />
           </div>
