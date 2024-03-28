18c923f343c4a554285d5f7b997e36bddd92981a
aditya-techstuff <aditya@techstuff.cloud>
2024-03-28 11:40:41 +0530

    Refactor login route to use Next.js API types

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,13 +1,17 @@
+import { NextApiRequest, NextApiResponse } from 'next';
 import { cookies } from 'next/headers';
 
-export async function POST(req: Request, res: Response) {
+export async function POST(req: Request, res: NextApiResponse) {
   // Store token in cookies to check authentication in middleware
-  cookies().set('token', 'random token', {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    maxAge: 60 * 60 * 24 * 7, // One week
-    path: '/',
-  });
+  const body = await req.json();
+  console.log(body);
+
+  // cookies().set('token', 'random token', {
+  //   httpOnly: true,
+  //   secure: process.env.NODE_ENV === 'production',
+  //   maxAge: 60 * 60 * 24 * 7, // One week
+  //   path: '/',
+  // });
 
   return new Response(JSON.stringify({ message: 'Signed-in successfully' }));
 }
