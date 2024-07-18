47944f07bda9d082d845015b6814386e95de9a55
chandresh-techstuff <chandresh@techstuff.cloud>
2024-07-18 17:48:44 +0530

    Add permissions and management routes

---
+++ app/management/layout.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ManagementLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <div>ManagementLayout</div>
+      {children}
+    </>
+  );
+}
---
+++ app/principle/layout.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function PrincipleLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <div>PrincipleLayout</div>
+      {children}
+    </>
+  );
+}
