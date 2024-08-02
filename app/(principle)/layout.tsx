84e141e5dd4766f295d38f517f494e2f80babcbd
chandresh-techstuff <chandresh@techstuff.cloud>
2024-08-02 19:18:12 +0530

    Add navbar in principle route

--- app/(principle)/layout.tsx
+++ app/(principle)/layout.tsx
@@ -1,42 +1,13 @@
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
 import React from 'react';
+import PrincipleNavbar from './navbar';
 
 export default function PrincipleLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <section className='p-10'>
       <div>PrincipleLayout</div>
+      <PrincipleNavbar />
 
-      <nav>
-        <Link href={'/principle/student'}>
-          <Button
-            size='sm'
-            variant='link'
-          >
-            View Students
-          </Button>
-        </Link>
-
-        <Link href={'/principle/teacher'}>
-          <Button
-            size='sm'
-            variant='link'
-          >
-            View Teachers
-          </Button>
-        </Link>
-
-        <Link href={'/principle/setting'}>
-          <Button
-            size='sm'
-            variant='link'
-          >
-            View Setting
-          </Button>
-        </Link>
-      </nav>
-
-      {children}
-    </>
+      <div className='mt-8'>{children}</div>
+    </section>
   );
 }
---
+++ app/(principle)/navbar.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import React from 'react';
+import { PRINCIPLE_PERMISSIONS } from '@/constants/permissions';
+import { usePermissions } from '@/lib/hooks/usePermissions';
+import Link from '@/components/Link';
+
+const PrincipleNavbar = () => {
+  const [hasViewLibraryPerms, hasViewStudentPerms, hasViewClassPerms, hasViewSettingPerms, hasViewTeacherPerms] =
+    usePermissions([
+      PRINCIPLE_PERMISSIONS.LIBRARY.VIEW,
+      PRINCIPLE_PERMISSIONS.STUDENT.VIEW,
+      PRINCIPLE_PERMISSIONS.CLASS.VIEW,
+      PRINCIPLE_PERMISSIONS.SETTING.VIEW,
+      PRINCIPLE_PERMISSIONS.TEACHER.VIEW,
+    ]);
+
+  return (
+    <nav>
+      <Link
+        href={'/principle/library'}
+        disabled={!hasViewLibraryPerms}
+      >
+        View Library
+      </Link>
+
+      <Link
+        href={'/principle/student'}
+        disabled={!hasViewStudentPerms}
+      >
+        View Students
+      </Link>
+
+      <Link
+        href={'/principle/class'}
+        disabled={!hasViewClassPerms}
+      >
+        View Class
+      </Link>
+
+      <Link
+        href={'/principle/setting'}
+        disabled={!hasViewSettingPerms}
+      >
+        View Setting
+      </Link>
+
+      <Link
+        href={'/principle/teacher'}
+        disabled={!hasViewTeacherPerms}
+      >
+        View Teacher
+      </Link>
+    </nav>
+  );
+};
+
+PrincipleNavbar.displayName = 'PrincipleNavbar';
+export default PrincipleNavbar;
