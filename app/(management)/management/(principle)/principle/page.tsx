227f8105a0f968a1ed7c65714f5de3c25eb84e81
chandresh-techstuff <chandresh@techstuff.cloud>
2024-08-02 18:36:02 +0530

    Add action buttons with permissions for Management Principle route

--- app/(management)/management/(principle)/principle/page.tsx
+++ app/(management)/management/(principle)/principle/page.tsx
@@ -1,7 +1,47 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { MANAGEMENT_STAFF_PERMISSIONS } from '@/constants/permissions';
+import { usePermissions } from '@/lib/hooks/usePermissions';
 import React from 'react';
 
 const ManagementPrinciplePage = () => {
-  return <div>Management principle page</div>;
+  const [hasAddPrinciplePerms, hasEditPrinciplePerms, hasDeletePrinciplePerms] = usePermissions([
+    MANAGEMENT_STAFF_PERMISSIONS.PRINCIPLE.ADD,
+    MANAGEMENT_STAFF_PERMISSIONS.PRINCIPLE.EDIT,
+    MANAGEMENT_STAFF_PERMISSIONS.PRINCIPLE.DELETE,
+  ]);
+
+  return (
+    <div className='flex justify-end gap-3'>
+      <Button
+        variant='destructive'
+        size='default'
+        disabled={!hasAddPrinciplePerms}
+        className={!hasAddPrinciplePerms ? 'cursor-not-allowed !pointer-events-auto' : ''}
+      >
+        Add Principle
+      </Button>
+
+      <Button
+        variant='destructive'
+        size='default'
+        disabled={!hasEditPrinciplePerms}
+        className={!hasEditPrinciplePerms ? 'cursor-not-allowed !pointer-events-auto' : ''}
+      >
+        Edit Principle
+      </Button>
+
+      <Button
+        variant='destructive'
+        size='default'
+        disabled={!hasDeletePrinciplePerms}
+        className={!hasDeletePrinciplePerms ? 'cursor-not-allowed !pointer-events-auto' : ''}
+      >
+        Delete Principle
+      </Button>
+    </div>
+  );
 };
 
 ManagementPrinciplePage.displayName = 'ManagementPrinciplePage';
