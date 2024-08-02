c981d8df79596f704ceea3af52eec5044ac18694
chandresh-techstuff <chandresh@techstuff.cloud>
2024-08-02 19:19:00 +0530

    Add action buttons with permissions for principle student route

--- app/(principle)/principle/(student)/student/page.tsx
+++ app/(principle)/principle/(student)/student/page.tsx
@@ -1,7 +1,47 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { PRINCIPLE_PERMISSIONS } from '@/constants/permissions';
+import { usePermissions } from '@/lib/hooks/usePermissions';
 import React from 'react';
 
 const PrincipleStudentPage = () => {
-  return <div>Principle student page</div>;
+  const [hasAddStudentPerms, hasEditStudentPerms, hasDeleteStudentPerms] = usePermissions([
+    PRINCIPLE_PERMISSIONS.STUDENT.ADD,
+    PRINCIPLE_PERMISSIONS.STUDENT.EDIT,
+    PRINCIPLE_PERMISSIONS.STUDENT.DELETE,
+  ]);
+
+  return (
+    <div className='flex justify-end gap-3'>
+      <Button
+        variant='destructive'
+        size='default'
+        disabled={!hasAddStudentPerms}
+        className={!hasAddStudentPerms ? 'cursor-not-allowed !pointer-events-auto' : ''}
+      >
+        Add Student
+      </Button>
+
+      <Button
+        variant='destructive'
+        size='default'
+        disabled={!hasEditStudentPerms}
+        className={!hasEditStudentPerms ? 'cursor-not-allowed !pointer-events-auto' : ''}
+      >
+        Edit Student
+      </Button>
+
+      <Button
+        variant='destructive'
+        size='default'
+        disabled={!hasDeleteStudentPerms}
+        className={!hasDeleteStudentPerms ? 'cursor-not-allowed !pointer-events-auto' : ''}
+      >
+        Delete Student
+      </Button>
+    </div>
+  );
 };
 
 PrincipleStudentPage.displayName = 'PrincipleStudentPage';
