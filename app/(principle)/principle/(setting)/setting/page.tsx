5f2d642ac3b402177b5c9fbe113ffd9b1730d150
chandresh-techstuff <chandresh@techstuff.cloud>
2024-08-02 19:18:49 +0530

    Add action buttons with permissions for principle setting route

--- app/(principle)/principle/(setting)/setting/page.tsx
+++ app/(principle)/principle/(setting)/setting/page.tsx
@@ -1,7 +1,47 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { PRINCIPLE_PERMISSIONS } from '@/constants/permissions';
+import { usePermissions } from '@/lib/hooks/usePermissions';
 import React from 'react';
 
 const PrincipleSettingPage = () => {
-  return <div>Principle setting page</div>;
+  const [hasAddSettingPerms, hasEditSettingPerms, hasDeleteSettingPerms] = usePermissions([
+    PRINCIPLE_PERMISSIONS.SETTING.ADD,
+    PRINCIPLE_PERMISSIONS.SETTING.EDIT,
+    PRINCIPLE_PERMISSIONS.SETTING.DELETE,
+  ]);
+
+  return (
+    <div className='flex justify-end gap-3'>
+      <Button
+        variant='destructive'
+        size='default'
+        disabled={!hasAddSettingPerms}
+        className={!hasAddSettingPerms ? 'cursor-not-allowed !pointer-events-auto' : ''}
+      >
+        Add Setting
+      </Button>
+
+      <Button
+        variant='destructive'
+        size='default'
+        disabled={!hasEditSettingPerms}
+        className={!hasEditSettingPerms ? 'cursor-not-allowed !pointer-events-auto' : ''}
+      >
+        Edit Setting
+      </Button>
+
+      <Button
+        variant='destructive'
+        size='default'
+        disabled={!hasDeleteSettingPerms}
+        className={!hasDeleteSettingPerms ? 'cursor-not-allowed !pointer-events-auto' : ''}
+      >
+        Delete Setting
+      </Button>
+    </div>
+  );
 };
 
 PrincipleSettingPage.displayName = 'PrincipleSettingPage';
