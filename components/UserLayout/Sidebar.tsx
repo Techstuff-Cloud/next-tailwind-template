0afa8f66270011dcf3814fc932d89a9041485d0f
Chandresh Patidar <chandresh@techstuff.cloud>
2024-08-28 14:49:02 +0530

    Show active link

--- components/UserLayout/Sidebar.tsx
+++ components/UserLayout/Sidebar.tsx
@@ -1,17 +1,33 @@
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { sidebarOptions } from './constants';
 import { SidebarItem } from './SidebarItem';
 
 const Sidebar = () => {
+  const pathname = usePathname();
+
+  const isActivePath = (path: string) => {
+    if (path === '/') {
+      return pathname === path; // Match exactly for root route
+    }
+    return pathname.startsWith(path);
+  };
+
   return (
     <div className='w-[268px] max-w-[268px] bg-transparent max-h-[calc(100vh_-_82px)] border-r border-r-surface-100'>
       <div className='h-full w-full p-2.5 box-border flex flex-col gap-40 justify-between  overflow-y-auto scrollbar'>
         <ul className='w-full flex flex-col gap-1.5'>
           {sidebarOptions.map((option) => (
-            <SidebarItem
+            <Link
+              href={option.href}
               key={option.id}
-              iconName={option.iconName}
-              label={option.label}
-            />
+            >
+              <SidebarItem
+                iconName={option.iconName}
+                label={option.label}
+                isActivePath={isActivePath(option.href)}
+              />
+            </Link>
           ))}
         </ul>
 
--- components/UserLayout/SidebarItem.tsx
+++ components/UserLayout/SidebarItem.tsx
@@ -1,19 +1,24 @@
+import { cn } from '@/lib/utils/utils';
 import { useMemo } from 'react';
 import { iconsMapping } from './constants';
 
 interface SidebarItemProps {
   iconName: string;
   label: string;
+  isActivePath?: boolean;
   onClick?: () => void;
 }
 
-export const SidebarItem = ({ iconName, label, onClick }: SidebarItemProps) => {
+export const SidebarItem = ({ iconName, label, isActivePath, onClick }: SidebarItemProps) => {
   const Icon = useMemo(() => iconsMapping[iconName as keyof typeof iconsMapping], [iconName]);
 
   return (
     <li
       onClick={onClick}
-      className='w-full flex items-center gap-3 p-3 rounded-lg text-gray-500 text-sm bg-transparent hover:bg-primary-50 hover:text-primary-500 hover:cursor-pointer'
+      className={cn(
+        'w-full flex items-center gap-3 p-3 rounded-lg text-gray-500 text-sm bg-transparent hover:cursor-pointer',
+        isActivePath ? 'bg-primary-500 text-gray-50' : 'hover:bg-primary-50 hover:text-primary-500'
+      )}
     >
       {Icon && (
         <Icon
